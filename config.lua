ba3a1e003a367d20647e91da9faaec1fedd18c5e
Lonestarzdev <urbanovainc@gmail.com>
2025-11-29 02:34:02 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+-- Cooldown time in seconds (1 hour = 3600)
+Config.Cooldown = 43200
+
+-- Player licenses authorized to use /drugplug
+-- Add entries like:
+-- ["license:xxxx"] = { item = "item_name", amount = 1 }
+
+Config.Plugs = {
+    -- Examples:
+   -- ["license:0410"] = { item = "ivpack", amount = 5000 },
+}
